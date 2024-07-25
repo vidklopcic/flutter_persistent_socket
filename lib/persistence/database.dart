8dfb93c21cdada122649577dc2f5d4ef9b9d4089
vidklopcic <klopcic.vid@gmail.com>
2024-07-25 12:49:54 +0200

    add missing override

--- lib/persistence/database.dart
+++ lib/persistence/database.dart
@@ -91,6 +91,11 @@ class _DummyExecutor implements QueryExecutor {
   Future<void> close() {
     throw UnimplementedError();
   }
+
+  @override
+  QueryExecutor beginExclusive() {
+    throw UnimplementedError();
+  }
 }
 
 Database database = Database();
